e306ce718386bf7818b904c54ddd2591b630535c
Stephen Pan <2899621320@qq.com>
2022-12-08 23:18:06 +0800

    Basically complete the approximate function

--- model/foo.crud.go
+++ model/foo.crud.go
@@ -11,12 +11,82 @@ func addCreateUrl(createUrl CreateURL) (uint, error) {
 	return createUrl.Id, err
 }
 
+// 新增用户信息
 func addUser(register Register) error {
 	err := DB.Create(Login{Email: register.Email, Pwd: register.Pwd}).Error
 	if err != nil {
 		return err
 	}
 	return err
+}
+
+// 验证登录信息是否有效
+func getLogin(login Login) (string, error) {
+	err := DB.Where("email = ? and pwd = ?", login.Email, login.Pwd).Error
+	if err != nil {
+		return "fail", err
+	}
+	return "successful", nil
+}
+
+// 查询短链接的详细信息
+func inquireURL(ids uint) (error, CreateURL) {
+	err := DB.Where("id = ?", "ids").Error
+	if err != nil {
+		return err, CreateURL{}
+	}
+	var createUrl CreateURL
+	err1 := DB.First(&createUrl, ids).Error
+	if err1 != nil {
+		return err1, CreateURL{}
+	}
+	return nil, createUrl
+}
+
+// 获取用户的所有的短链接
+func getUserAllURL(emails string) (error, []CreateURL) {
+	err := DB.Where("email = ?", "emails").Error
+	if err != nil {
+		return err, []CreateURL{}
+	}
+	var createUrl []CreateURL
+	err1 := DB.Find(&createUrl, emails).Error
+	if err1 != nil {
+		return err1, []CreateURL{}
+	}
+	return nil, createUrl
+}
+
+// UpdateShortURL 更新短链接
+func updateShortURL(ids uint, update UpdateURL) error {
+	err := DB.Where("id = ?", "ids").Error
+	if err != nil {
+		return err
+	}
+	var newURLInfo CreateURL
+	err1 := DB.First(&newURLInfo, ids).Error
+	if err1 != nil {
+		return err1
+	}
+	newURLInfo.Origin = update.Origin
+	newURLInfo.Comment = update.Comment
+	newURLInfo.StartTime = update.StartTime
+	newURLInfo.ExpireTime = update.ExpireTime
+	DB.Save(&newURLInfo)
+	return nil
+}
+
+// 删除短链接
+func deleteShortUrl(ids uint) error {
+	err := DB.Where("id = ?", "ids").Error
+	if err != nil {
+		return err
+	}
+	err1 := DB.Delete(&CreateURL{}, ids).Error
+	if err1 != nil {
+		return err1
+	}
+	return nil
 }
 
 /*
@@ -39,16 +109,8 @@ func addLogin(login Login) error {
 }
 */
 
-// 验证登录信息是否有效
-func getLogin(login Login) (string, error) {
-	err := DB.Where("email = ? and pwd = ?", login.Email, login.Pwd).Error
-	if err != nil {
-		return "fail", err
-	}
-	return "successful", nil
-}
-
 // 获取用户信息
+/*
 func getUserInfo(register Register) (error, ReturnInfo) {
 	err := DB.Model(&register).Where("email = ? and pwd = ?", register.Email, register.Pwd).Error
 	if err != nil {
@@ -59,17 +121,4 @@ func getUserInfo(register Register) (error, ReturnInfo) {
 	returnInfo.Email = register.Email
 	return nil, returnInfo
 }
-
-// 查询短链接的详细信息
-func inquireURL(ids uint) (error, CreateURL) {
-	err := DB.Where("id = ?", "ids").Error
-	if err != nil {
-		return err, CreateURL{}
-	}
-	var createUrl CreateURL
-	err1 := DB.First(&createUrl, ids).Error
-	if err1 != nil {
-		return err1, CreateURL{}
-	}
-	return nil, createUrl
-}
+*/
