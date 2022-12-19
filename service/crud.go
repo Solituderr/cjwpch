ff70769b5ede393dff3f1dac077138589bf2e884
Solitude <caijuwnu@gmail.com>
2022-12-19 15:44:53 +0800

    添加dao层接口

--- service/crud.go
+++ service/crud.go
@@ -7,20 +7,18 @@ import (
 type LinkDeal interface {
 	CreateUrl(model.CreateURL) (uint, error)
 	CreateUrlLogin(model.CreateURL, uint) (uint, error) //id
-	InquireUrl(uint) (model.CreateURL, error)
-	UpdateUrl(model.UpdateURL) error
+	InquireUrl(uint) (model.Link, error)
+	UpdateUrl(uint, model.UpdateURL) error
 	DeleteUrl(uint) error
 	PauseUrl(uint) error
 	GetUrl(string) (string, error) //传短链接，返回重定向到的链接
 }
 
 type UserDeal interface {
-	AddUser(model.Register) error          //创建用户，返回是否有错误
-	CheckUser(model.Login) (string, error) //返回success或fail  判断登录是否成功
-	LogOutUser() error
-	GetInfoUser(string) (string, string, error)
-
-	GetUrlsUser(string) ([]model.CreateURL, error)
+	AddUser(model.Register) error                //创建用户，返回是否有错误
+	CheckUser(model.Login) (string, uint, error) //返回success或fail  判断登录是否成功
+	GetInfoUser(string) (string, string, error)  //传email 返回name和pwd
+	GetUrlsUser(string) ([]model.Link, error)
 }
 
 type CRUD interface {
@@ -31,60 +29,58 @@ type CRUD interface {
 type Deal struct{}
 
 func (Deal) CreateUrl(url model.CreateURL) (uint, error) {
-	//TODO implement me
-	panic("implement me")
+	id, err := model.AddLink(url)
+	return id, err
 }
 
 func (Deal) CreateUrlLogin(url model.CreateURL, id uint) (uint, error) {
-	//TODO implement me
-	panic("implement me")
+	loginId, err := model.AddLinkLogin(url, id)
+	return loginId, err
 }
 
-func (Deal) InquireUrl(id uint) (model.CreateURL, error) {
-	//TODO implement me
-	panic("implement me")
+func (Deal) InquireUrl(id uint) (model.Link, error) {
+	err, link := model.InquireURL(id)
+	return link, err
 }
 
-func (Deal) UpdateUrl(link model.UpdateURL) error {
-	//TODO implement me
-	panic("implement me")
+func (Deal) UpdateUrl(id uint, link model.UpdateURL) error {
+	err := model.UpdateShortURL(id, link)
+	return err
 }
 
 func (Deal) DeleteUrl(id uint) error {
-	//TODO implement me
-	panic("implement me")
+	err := model.DeleteShortUrl(id)
+	return err
 }
 
 func (Deal) PauseUrl(id uint) error {
-	//TODO implement me
-	panic("implement me")
+	err := model.PauseUrl(id)
+	return err
 }
 
 func (Deal) GetUrl(s string) (string, error) {
-	panic("123")
+	url, err := model.GetUrl(s)
+	return url, err
 }
+
+//------------------------------------------------------------//
 
 func (Deal) AddUser(register model.Register) error {
-	//TODO implement me
-	panic("implement me")
+	err := model.AddUser(register)
+	return err
 }
 
-func (Deal) CheckUser(login model.Login) (string, error) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (Deal) LogOutUser() error {
-	//TODO implement me
-	panic("implement me")
+func (Deal) CheckUser(login model.Login) (string, uint, error) {
+	isSuccess, id, err := model.GetLogin(login)
+	return isSuccess, id, err
 }
 
 func (Deal) GetInfoUser(s string) (string, string, error) {
-	//TODO implement me
-	panic("implement me")
+	name, pwd, err := model.GetInfoUser(s)
+	return name, pwd, err
 }
 
-func (Deal) GetUrlsUser(s string) ([]model.CreateURL, error) {
-	//TODO implement me
-	panic("implement me")
+func (Deal) GetUrlsUser(s string) ([]model.Link, error) {
+	err, links := model.GetUserAllURL(s)
+	return links, err
 }
