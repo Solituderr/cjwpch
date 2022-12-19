df1ea5dd8a65d255dbb77209156027a3879c295d
Solitude <caijuwnu@gmail.com>
2022-12-19 15:44:16 +0800

    添加短链接算法

---
+++ service/shorturl_gen.go
@@ -0,0 +1,61 @@
+package service
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"strconv"
+)
+
+const (
+	VAL   = 0x3FFFFFFF
+	INDEX = 0x0000003D
+)
+
+var (
+	alphabet = []byte("abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+)
+
+// Transform 短链接生成算法
+func Transform(longURL string) (string, error) {
+	md5Str := getMd5Str(longURL)
+	//var hexVal int64
+	var tempVal int64
+	var result [4]string
+	var tempUri []byte
+	var output string
+	output = "https://shorturl.com/"
+	for i := 0; i < 4; i++ {
+		tempSubStr := md5Str[i*8 : (i+1)*8]
+		hexVal, err := strconv.ParseInt(tempSubStr, 16, 64)
+		if err != nil {
+			return "", nil
+		}
+		tempVal = int64(VAL) & hexVal
+		var index int64
+		tempUri = []byte{}
+		for i := 0; i < 6; i++ {
+			index = INDEX & tempVal
+			tempUri = append(tempUri, alphabet[index])
+			tempVal = tempVal >> 5
+		}
+		result[i] = string(tempUri)
+		output += result[i]
+	}
+	return output, nil
+}
+
+// 以十六进制格式生成 URL 的 md5 校验和
+func getMd5Str(str string) string {
+	m := md5.New()
+	m.Write([]byte(str))
+	c := m.Sum(nil)
+	return hex.EncodeToString(c)
+}
+
+/*
+func main() {
+	longUrl := "https://www.assemblyai.com/blog/diffusion-models-for-machine-learning-introduction/"
+	output, _ := Transform(longUrl)
+	fmt.Println(output)
+}
+*/
